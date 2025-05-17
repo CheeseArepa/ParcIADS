aabc95b594a2182b36150ef8939fe113bb436a7c
desotoangeline6 <desotoangeline6@gmail.com>
2025-05-17 14:49:17 +0000

    menu principal

---
+++ main.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+    print("\n===== MENÚ DE OPCIONES =====")
+    print("1. Registrar orden")
+    print("2. Ver fila")
+    print("3. Ver estadísticas")
+    print("4. Reiniciar datos")
+    print("5. Salir")
+    print("============================\n")
