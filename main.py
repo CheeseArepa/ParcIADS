fde9a9ffcd9338a013f0a224fc9ea5ea656c686a
mendozamunozkevinalejandro <mendozamunozkevinalejandro@gmail.com>
2025-05-17 15:59:09 +0000

    Inventario

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ plantilla_producto = {
     "cantidad": 0
 }
 
+productos_base: dict[int, dict] = {
+    1: {**plantilla_producto, "id": 1, "nombre": "Lay's", "precio": 1000, "cantidad": 5},
+    2: {**plantilla_producto, "id": 2, "nombre": "Doritos", "precio": 1500, "cantidad": 5},
+    3: {**plantilla_producto, "id": 3, "nombre": "Choclitos", "precio": 500, "cantidad": 5},
+    4: {**plantilla_producto, "id": 4, "nombre": "Cheetos", "precio": 900, "cantidad": 5},
+    5: {**plantilla_producto, "id": 5, "nombre": "Mani", "precio": 500, "cantidad": 5},
+    6: {**plantilla_producto, "id": 6, "nombre": "Mani Moto", "precio": 700, "cantidad": 5},
+    7: {**plantilla_producto, "id": 7, "nombre": "Mani con pasas", "precio": 600, "cantidad": 5},
+    8: {**plantilla_producto, "id": 8, "nombre": "Mani Mixto", "precio": 800, "cantidad": 5},
+    9: {**plantilla_producto, "id": 9, "nombre": "Gala", "precio": 1000, "cantidad": 5},
+    10: {**plantilla_producto, "id": 10, "nombre": "Chocorramo", "precio": 1200, "cantidad": 5},
+    11: {**plantilla_producto, "id": 11, "nombre": "Gansito", "precio": 900, "cantidad": 5},
+    12: {**plantilla_producto, "id": 12, "nombre": "Browni", "precio": 2500, "cantidad": 5},
+    13: {**plantilla_producto, "id": 13, "nombre": "Pepsi", "precio": 1500, "cantidad": 5},
+    14: {**plantilla_producto, "id": 14, "nombre": "Manzana", "precio": 1500, "cantidad": 5},
+    15: {**plantilla_producto, "id": 15, "nombre": "Colombiana", "precio": 1500, "cantidad": 5},
+    16: {**plantilla_producto, "id": 16, "nombre": "Uva", "precio": 1500, "cantidad": 5},
+
+}
+productos = productos_base.copy()
+
+
 def leer_entero(prompt: str, minimo: int = None, maximo: int = None) -> int:
     while True:
         try:
@@ -25,3 +47,4 @@ def mostrar_menu() -> int:
     print("5. Salir")
     print("============================\n")
     return leer_entero("Seleccione una opción (1-5): ", minimo=1, maximo=5)
+    
